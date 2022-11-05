ba9cc7ec0aea8d1aaffa62107d1766f5262f70ac
zayaanw <zayaanw@gmail.com>
2022-11-05 19:08:56 +0000

    finishing touches

---
+++ README.md
@@ -0,0 +1,3 @@
+## Unit Converter
+
+Simple unit convertor that outputs length, volume and mass conversions when supplied a value. Made as part of the solo project at [Scrimba](https://scrimba.com/learn/learnjavascript/solo-project-unit-converter-cz9aPNSr).
--- index.js
+++ index.js
@@ -6,12 +6,24 @@
 
 const convertBtn = document.querySelector("#convert-btn");
 const lengthconversionEl = document.querySelector("#lengthconversion-el");
+const volumeconversionEl = document.querySelector("#volumeconversion-el");
+const massconversionEl = document.querySelector("#massconversion-el");
 const num = document.querySelector("#num");
 
 convertBtn.addEventListener("click", function () {
   let lengthstring = "";
+  let volumestring = "";
+  let massstring = "";
   lengthstring = `${num.value} meters = ${(num.value * 3.281).toFixed(
     3
   )} feet | ${num.value} feet = ${(num.value / 3.281).toFixed(3)} meters`;
+  volumestring = `${num.value} liters = ${(num.value * 0.264).toFixed(
+    3
+  )} gallons | ${num.value} gallons = ${(num.value / 0.264).toFixed(3)} liters`;
+  massstring = `${num.value} kilos = ${(num.value * 2.204).toFixed(
+    3
+  )} pounds | ${num.value} pounds = ${(num.value / 2.204).toFixed(3)} kilos`;
   lengthconversionEl.innerHTML = lengthstring;
+  volumeconversionEl.innerHTML = volumestring;
+  massconversionEl.innerHTML = massstring;
 });
